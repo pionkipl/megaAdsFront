e9fad8f94cf3469fa3d77ce5ad7b117ee9b87dd1
Jarek <j.szczepaniak@albedomarketing.pl>
2022-05-14 12:38:19 +0200

    Add leaflet map.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon';
 
+import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
     return (
-        <div className='map'/>
+        <div className='map'>
+            <MapContainer center={[52.4231573,16.9377912]} zoom={15}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[52.4231573,16.9377912]}>
+                    <Popup>
+                        <h1>Park cytadela</h1>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     )
 }
