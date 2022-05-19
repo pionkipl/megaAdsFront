712b82f1864d7ad02818796cfe90bbe5f159bd40
Jarek <j.szczepaniak@albedomarketing.pl>
2022-05-19 12:56:48 +0200

    Map and markers are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {SearchContext} from "../../contexts/search.context";
 import '../../utils/fix-map-icon';
+import {SimpleAdEntity} from 'types';
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
+import {SingleAd} from "./SingleAd";
 
 export const Map = () => {
+    const { search } = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([])
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch(`http://localhost:3001/ad/search/${search }`);
+            const data = await res.json();
+
+            setAds(data);
+
+        })();
+    }, [search])
+
     return (
         <div className='map'>
             <MapContainer center={[52.4231573,16.9377912]} zoom={15}>
@@ -13,11 +29,15 @@ export const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[52.4231573,16.9377912]}>
-                    <Popup>
-                        <h1>Park cytadela</h1>
-                    </Popup>
-                </Marker>
+                {
+                    ads.map(ad => (
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <SingleAd id={ad.id }/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
             </MapContainer>
         </div>
     )
